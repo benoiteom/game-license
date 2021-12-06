20e7fb3e1b479539e2cdf59528333a11e556b16f
beo2 <beo2@illinois.edu>
2021-12-06 14:53:21 -0700

    fix: games/index.js

---
+++ pages/api/index.js
@@ -0,0 +1,16 @@
+import { ref, child, get } from "firebase/database";
+import { db } from '../../../firebase';
+
+export default async function handler(req, res) {
+    const dbRef = ref(db);
+    await get(child(dbRef, `games`)).then((snapshot) => {
+        if (snapshot.exists()) {
+            let games = snapshot.val();
+            res.status(200).json(games);
+        } else {
+            res.status(404).json({ status: 'Not Found' });
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
+}
