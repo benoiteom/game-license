76aba4ba0ab90a676a78ffc0f247fe0902cf01fa
beo2 <beo2@illinois.edu>
2021-12-14 13:58:51 -0700

    fix: small footer links and remove copyright for site

--- components/Footers/FooterSmall.js
+++ components/Footers/FooterSmall.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link';
 
 export default function FooterSmall(props) {
   return (
@@ -15,7 +16,7 @@ export default function FooterSmall(props) {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
               <div className="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
-                Copyright © {new Date().getFullYear()}{" "}
+                {/* Copyright © {new Date().getFullYear()}{" "} */}
                 <a
                   href="https://www.creative-tim.com?ref=nnjs-footer-small"
                   className="text-white hover:text-blueGray-300 text-sm font-semibold py-1"
@@ -27,28 +28,25 @@ export default function FooterSmall(props) {
             <div className="w-full md:w-8/12 px-4">
               <ul className="flex flex-wrap list-none md:justify-end  justify-center">
                 <li>
-                  <a
-                    href="https://www.creative-tim.com/presentation?ref=nnjs-footer-small"
-                    className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                  >
-                    About Us
-                  </a>
+                  <Link href='/about'>
+                    <a className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3" >
+                      About Us
+                    </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="http://blog.creative-tim.com?ref=nnjs-footer-small"
-                    className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                  >
-                    Contact Us
-                  </a>
+                  <Link href='/about'>
+                    <a className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3" >
+                      Contact Us
+                    </a>
+                  </Link>
                 </li>
                 <li>
-                  <a
-                    href="https://github.com/creativetimofficial/notus-nextjs/blob/main/LICENSE.md?ref=nnjs-footer-small"
-                    className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3"
-                  >
-                    Home
-                  </a>
+                  <Link href='/'>
+                    <a className="text-white hover:text-blueGray-300 text-sm font-semibold block py-1 px-3" >
+                      Home
+                    </a>
+                  </Link>
                 </li>
               </ul>
             </div>
